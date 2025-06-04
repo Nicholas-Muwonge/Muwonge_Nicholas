fe84536aeb9285e600ea45a41a62d5bcebdbbb3b
Nicholas-Muwonge <klausnicholas20@gmail.com>
2025-06-04 09:43:45 +0300

    data

---
+++ MACHINE LEARNING/learn_machinelearning.py
@@ -0,0 +1,21 @@
+#types of machine learning
+#1. supervised and unsupervised learning
+
+#Supervised learning -labeled data
+'''
+-random forest
+-k nearest neighbour(knn)
+-decision trees
+-logistic regression
+-linear regression
+-support vector machines
+deep learning(cnn) - image classification
+'''
+
+#unsupervised learning -unlabeled data
+'''
+-hierarchial clustering
+-kmeans clustering
+
+'''
+#semi supervised learning
