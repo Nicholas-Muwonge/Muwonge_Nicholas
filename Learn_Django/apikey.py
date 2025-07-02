9b4174a448e51f0cb615013070476fa1f887526a
Nicholas-Muwonge <klausnicholas20@gmail.com>
2025-07-02 16:11:10 +0300

    api that returns details from the lat and long of kampala

--- Learn_Django/apikey.py
+++ Learn_Django/apikey.py
@@ -1,5 +1,3 @@
-# Creating an API key for the project
-# This is used to authenticate the user and allow them to access the API
 import os
 import pandas as pd
 import requests
@@ -8,20 +6,15 @@ import requests
 lat = 0.347596
 lon = 32.582520 
 
-# Your API key
 API_KEY = 'f0492ea08895bb2f9520ae51a06712f3'
 
-# URL with the defined variables
-url = f"http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=a0f0aa5d298957f90a2283b6838600d2"
+url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric"
 
-# GET request
 response = requests.get(url)
 
-# Check if the request was successful
 if response.status_code == 200:
     data = response.json()
     
-    # save the data to a CSV file
     df = pd.DataFrame([data])
     df.to_csv('weather_data.csv', index=False)
     print("Data saved to weather_data.csv")
